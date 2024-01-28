5cc60de9fded87412c83a9aafc1cd91f6848fdde
teja-patnala <patnalatejaa@gmail.com>
2024-01-28 11:17:29 +0530

    Your commit message here

--- Server/app.js
+++ Server/app.js
@@ -1,20 +1,17 @@
-const express = require('express');
+// server.js
+
+const express = require("express");
 const app = express();
 const port = 3000;
 const cors = require("cors");
 require("dotenv").config();
 
-app.use(express.json());  
-app.use(cors());
-const bodyParser = require('body-parser');
+app.use(express.json());
+app.use(cors({ origin: 'http://localhost:5173' }));
 
-// Parse JSON and URL-encoded form data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); 
-
-//Razorpay Route
-const phonepeRoute = require('./phonepay/phonepeController')
-app.use("/api", phonepeRoute);
+// Razorpay Route
+const phonepeRoute = require('./routes/phonepe/phonepeRoute');
+app.use("/api/payment/phonepe", phonepeRoute);
 
 // Starting Server
 app.listen(port, () => {
